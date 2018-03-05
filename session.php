1bc3115fa4bc636259aca404dfd36a629ff7422d
Erik Figueiredo <erik.figueiredo@gmail.com>
2018-03-05 05:35:09 -0300

    aulas 9

--- session.php
+++ session.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('session.save_handler', 'files');
+ini_set('session.save_path', __DIR__.'/sessions');
+
 session_set_cookie_params(60*60*24*365*99, '/', 'localhost', false, true);
 
 session_start();
